332a48d425f1b334f00eba19f0d6b189bb16719d
eliashezron <eliashezron23@gmail.com>
2022-12-15 08:55:01 +0300

    commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,41 +1,16 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
-import reportWebVitals from "./reportWebVitals"
-import { ChakraProvider, extendTheme } from "@chakra-ui/react"
-import { Mainnet, DAppProvider, Config, Goerli } from "@usedapp/core"
-import { BrowserRouter } from "react-router-dom"
+import React from "react";
+import ReactDOM from "react-dom";
+import { DAppProvider } from "@usedapp/core";
 
-const theme = extendTheme({
-  config: {
-    initialColorMode: "dark",
-  },
-})
-const config: Config = {
-  readOnlyChainId: Mainnet.chainId,
-  readOnlyUrls: {
-    [Mainnet.chainId]:
-      "https://mainnet.infura.io/v3/379d2d85420a445cb0f197f6c7b01977",
-    [Goerli.chainId]:
-      "https://goerli.infura.io/v3/379d2d85420a445cb0f197f6c7b01977",
-  },
-  notifications: {
-    expirationPeriod: 10000,
-    checkInterval: 1000,
-  },
-}
+import App from "./App";
+import { DAPP_CONFIG } from "./config";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <DAppProvider config={config}>
-      <ChakraProvider theme={theme}>
-        <BrowserRouter>
-          <App isServerInfo />
-        </BrowserRouter>
-      </ChakraProvider>
+    <DAppProvider config={DAPP_CONFIG}>
+      <App />
     </DAppProvider>
   </React.StrictMode>,
   document.getElementById("root")
-)
-
-reportWebVitals()
+);
